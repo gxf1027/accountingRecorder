0dad55011d9607a1f18941ad64c032acfe4a4dd5
gxf1027 <36832716+gxf1027@users.noreply.github.com>
2018-07-19 22:05:23 +0800

    bug fix
    	@Autowired（fix+）
    	private FinanProductsNoticeDao noticeDao;

--- KeepAccounts/src/cn/gxf/spring/quartz/job/service/RpcService.java
+++ KeepAccounts/src/cn/gxf/spring/quartz/job/service/RpcService.java
@@ -1,7 +1,6 @@
 package cn.gxf.spring.quartz.job.service;
 
-import java.util.Map;
 
 public interface RpcService {
-	public void SetFinanProductsNoticeMailed(String uuid); // 以批次号和用户id作为联合主键的表
+	public void setFinanProductsNoticeMailed(String uuid);
 }
--- KeepAccounts/src/cn/gxf/spring/quartz/job/service/RpcServiceImpl.java
+++ KeepAccounts/src/cn/gxf/spring/quartz/job/service/RpcServiceImpl.java
@@ -1,15 +1,18 @@
 package cn.gxf.spring.quartz.job.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import cn.gxf.spring.quartz.job.dao.FinanProductsNoticeDao;
 
 public class RpcServiceImpl implements RpcService{
 
+	@Autowired
 	private FinanProductsNoticeDao noticeDao;
 	
 	@Override
-	public void SetFinanProductsNoticeMailed(String uuid) {
+	public void setFinanProductsNoticeMailed(String uuid) {
 		System.out.println("SetFinanProductsNoticeMailed..." + uuid);
-		//noticeDao.setMailed(uuid);
+		noticeDao.setMailed(uuid);
 	}
 
 }
