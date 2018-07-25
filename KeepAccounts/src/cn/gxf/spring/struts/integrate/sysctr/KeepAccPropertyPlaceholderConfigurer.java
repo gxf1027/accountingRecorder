ff4a78be7ae0b4374ae075d400a17182390fe3e8
gxf1027 <36832716+gxf1027@users.noreply.github.com>
2018-07-25 20:12:30 +0800

    配置文件密码加密
    采用继承PropertyPlaceholderConfigurer类的方式+AES对称加密

---
+++ KeepAccounts/src/cn/gxf/spring/struts/integrate/sysctr/KeepAccPropertyPlaceholderConfigurer.java
@@ -0,0 +1,29 @@
+package cn.gxf.spring.struts.integrate.sysctr;
+
+import java.util.Enumeration;
+import java.util.Properties;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import cn.gxf.spring.struts.integrate.util.AESUtil;
+
+public class KeepAccPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
+
+	@Override
+	public void processProperties(ConfigurableListableBeanFactory beanFactory, Properties props) {
+		Enumeration<?> keys = props.propertyNames();
+		while (keys.hasMoreElements()) {
+			String key = (String) keys.nextElement();
+			String value = props.getProperty(key);
+			if (key.endsWith(".encryption") && null != value) {
+				props.remove(key);
+				key = key.substring(0, key.length() - 11);
+				// Ω‚√‹
+				value = AESUtil.decrypt(value.trim());
+				props.setProperty(key, value);
+			}
+			System.setProperty(key, value);
+		}
+
+		super.processProperties(beanFactory, props);
+	}
+}
