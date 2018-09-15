7f0573a6eb27b1eb8b7792576201fe831d528eb3
gxf1027 <36832716+gxf1027@users.noreply.github.com>
2018-09-15 10:36:19 +0800

    redis list listener小修改

--- keepaccounts-main/src/main/java/cn/gxf/spring/struts/integrate/cache/RedisMessageListener.java
+++ keepaccounts-main/src/main/java/cn/gxf/spring/struts/integrate/cache/RedisMessageListener.java
@@ -5,22 +5,20 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
 
 import cn.gxf.spring.motan.filter.FilterConstants;
 import cn.gxf.spring.motan.mbdao.RpcRequestLogDao;
 import cn.gxf.spring.motan.model.RpcRequestInfo;
-import cn.gxf.spring.struts.mybatis.dao.AccountVoMBDao;
 
 
 public class RedisMessageListener implements MessageListener{
 
 	private Logger logger = LogManager.getLogger();
 	
+	private String listenerName;
 	private RedisTemplate<String, Object> redisTemplate;  
 	private RpcRequestLogDao rpcRequestLogDao;
 	
@@ -44,33 +42,49 @@ public class RedisMessageListener implements MessageListener{
 		long commond_num = popnum;
 		while(popnum > 0){
 			
-			if (redisTemplate.opsForList().size(FilterConstants.RPC_REQUEST_LIST) == 0){
-				break;
+			// 以防止在循环中出现异常导致listener挂掉
+			try {
+				if (redisTemplate.opsForList().size(FilterConstants.RPC_REQUEST_LIST) == 0){
+					break;
+				}
+				
+				// 这里直接就反序列化好了
+				RpcRequestInfo rpcReq =  (RpcRequestInfo) redisTemplate.opsForList().rightPop(FilterConstants.RPC_REQUEST_LIST);
+				// 多个订阅客户端情况下需要考虑,可能先被其他客户端pop完
+				if (null == rpcReq){
+					break;
+				}
+				rpcReqList.add(rpcReq);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				logger.info("RedisMessageListener exception in loop: " + e.getMessage());
 			}
 			
-			// 这里直接就反序列化好了
-			RpcRequestInfo rpcReq =  (RpcRequestInfo) redisTemplate.opsForList().rightPop(FilterConstants.RPC_REQUEST_LIST);
-			// 多个订阅客户端情况下需要考虑,可能先被其他客户端pop完
-			if (null == rpcReq){
-				break;
-			}
-			rpcReqList.add(rpcReq);
-			 
-			popnum--;
+			popnum--; // 即使抛出异常也要--, 否则可能死循环
+			
 		}
 		
-		logger.info("RedisMessageListener pop out " + (commond_num - popnum) +" RpcRequestInfo objects. \n Consume " + (System.currentTimeMillis()-start) + " millis");
-		
+		logger.info(String.format("RedisMessageListener '%s' has %s RpcRequestInfo objects need to pop out", this.listenerName, commond_num));
+		logger.info(String.format("RedisMessageListener '%s' pops out %s RpcRequestInfo objects.  Consume %s millis.", this.listenerName, (commond_num - popnum), (System.currentTimeMillis()-start)));		
 		// 持久化到mysql
 		if (rpcReqList.size() > 0){
 			try {
 				rpcRequestLogDao.saveRequestsInfo(rpcReqList);
-				logger.info("RedisMessageListener persistent RpcRequestInfo Objects.");
+				logger.info(String.format("RedisMessageListener '%s' persistent RpcRequestInfo Objects", this.listenerName));
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 		
+	}
+	
+	public String getListenerName() {
+		return listenerName;
+	}
+	
+	public void setListenerName(String listenerName) {
+		this.listenerName = listenerName;
 	}
 
 	public RedisTemplate<String, Object> getRedisTemplate() {
