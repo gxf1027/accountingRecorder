02085a3293d66267e1327f941c0f5d91be60277f
gxf1027 <36832716+gxf1027@users.noreply.github.com>
2018-11-24 10:39:03 +0800

    增加mybatis的拦截器（计算sql运行时间）

---
+++ keepaccounts-main/src/main/java/cn/gxf/spring/struts/mybatis/config/interceptors/SqlCostInterceptor.java
@@ -0,0 +1,50 @@
+package cn.gxf.spring.struts.mybatis.config.interceptors;
+
+import org.apache.ibatis.plugin.Signature;
+
+import java.sql.Statement;
+import java.util.Properties;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.session.ResultHandler;
+
+/**
+ * Sql执行时间记录拦截器
+ */
+
+@Intercepts({
+		@Signature(type = StatementHandler.class, method = "query", args = { Statement.class, ResultHandler.class }),
+		@Signature(type = StatementHandler.class, method = "update", args = { Statement.class }),
+		@Signature(type = StatementHandler.class, method = "batch", args = { Statement.class }) })
+public class SqlCostInterceptor implements Interceptor {
+
+	@Override
+	public Object intercept(Invocation invocation) throws Throwable {
+		long startTime = System.currentTimeMillis();
+		try {
+			return invocation.proceed();
+		} finally {
+			long endTime = System.currentTimeMillis();
+			long sqlCost = endTime - startTime;
+			System.out.println("执行耗时 : [" + sqlCost + "ms ] ");
+		}
+	}
+
+	@Override
+	public Object plugin(Object target) {
+		
+		return Plugin.wrap(target, this);
+
+	}
+
+	@Override
+	public void setProperties(Properties arg0) {
+		
+
+	}
+
+}
