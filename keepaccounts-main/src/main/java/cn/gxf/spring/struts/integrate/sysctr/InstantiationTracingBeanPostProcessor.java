2a18f2d41ae976f80e6e1f6931f26c6975c55b53
gxf1027 <36832716+gxf1027@users.noreply.github.com>
2019-04-19 20:28:21 +0800

    上个版本log4j2编程式配置在启动时加载

--- keepaccounts-main/src/main/java/cn/gxf/spring/struts/integrate/sysctr/InstantiationTracingBeanPostProcessor.java
+++ keepaccounts-main/src/main/java/cn/gxf/spring/struts/integrate/sysctr/InstantiationTracingBeanPostProcessor.java
@@ -6,6 +6,8 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import com.weibo.api.motan.common.MotanConstants;
 import com.weibo.api.motan.util.MotanSwitcherUtil;
 
+import cn.gxf.spring.struts.integrate.sysctr.log.Log4j2Config;
+
 public class InstantiationTracingBeanPostProcessor implements ApplicationListener<ContextRefreshedEvent> {
 
 	@Override
@@ -16,6 +18,8 @@ public class InstantiationTracingBeanPostProcessor implements ApplicationListene
         	// server程序启动后，需要显式调用心跳开关，注册到consul
         	MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
     		System.out.println("InstantiationTracingBeanPostProcessor.....显式调用心跳开关，注册到consul.");
+    		
+    		Log4j2Config.config();
         }
 		
 	}
